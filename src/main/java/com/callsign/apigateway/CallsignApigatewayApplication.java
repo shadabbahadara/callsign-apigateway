04d2c747a3cec4d99134e71befd7642f66d61f83
shadab.khan@ihsmarkit.com <shadab.khan@ihsmarkit.com>
2022-01-09 22:54:19 +0530

    Initial commit

---
+++ src/main/java/com/callsign/apigateway/CallsignApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.callsign.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallsignApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallsignApigatewayApplication.class, args);
+    }
+
+}
